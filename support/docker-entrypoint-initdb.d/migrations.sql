18e9ba0cbb33a2300325b191e296e3e2ba20f861
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2020-05-05 12:52:18 +1000

    Fix PostgreSQL in CI

--- support/docker-entrypoint-initdb.d/migrations.sql
+++ support/docker-entrypoint-initdb.d/migrations.sql
@@ -1,10 +1,10 @@
--- How this file was generated:
--- * `cd spec/dummy; bundle; bundle exec rake db:migrate`
--- * `pg_dump -h 127.0.0.1 -U postgcres -W part_test`
-
+-- This file was generated using the `spec/dummy` Rails app that was removed
+-- before v3.0.0.
 --
--- Name: schema_migrations; Type: TABLE; Schema: public; Owner: postgres
+--     cd spec/dummy; bundle; bundle exec rake db:migrate
+--     pg_dump -h 127.0.0.1 -U postgcres -W part_test
 --
+-- https://github.com/jobseekerltd/partitioned/blob/v2.1.0/spec/dummy
 
 CREATE TABLE public.schema_migrations (
     version character varying NOT NULL
@@ -13,24 +13,10 @@ CREATE TABLE public.schema_migrations (
 
 ALTER TABLE public.schema_migrations OWNER TO postgres;
 
---
--- Data for Name: schema_migrations; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.schema_migrations (version) FROM stdin;
-\.
-
-
---
--- Name: unique_schema_migrations; Type: INDEX; Schema: public; Owner: postgres
---
+-- COPY public.schema_migrations (version) FROM stdin;
+-- \.
 
 CREATE UNIQUE INDEX unique_schema_migrations ON public.schema_migrations USING btree (version);
-
-
---
--- Name: SCHEMA public; Type: ACL; Schema: -; Owner: postgres
---
 
 REVOKE ALL ON SCHEMA public FROM PUBLIC;
 REVOKE ALL ON SCHEMA public FROM postgres;
